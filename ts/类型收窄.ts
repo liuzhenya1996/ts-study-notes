d9b81862e104d6ba3814a96d382a41f7800badaa
刘振亚 <1965350918@qq.com>
2023-04-30 17:44:10 +0800

    使用类型陈述语句收窄

--- ts/类型收窄.ts
+++ ts/类型收窄.ts
@@ -13,3 +13,32 @@ function getFixed (params?: number) {
 function getEqul (params1: number | string, params2: number | boolean) {
   if (params1 === params2) return params1 + params2 // params1 和 params2相等时，必然都是 number 类型
 }
+
+// 4.使用 in 或 instanceof 收窄
+type Fish = {
+  swim: string
+}
+
+type Bird = {
+  fly: string
+}
+
+function test1 (params: Fish | Bird) {
+  if ('swin' in params) {
+    return params.swin
+  }
+}
+
+function test2 (params: Date | String) {
+  if (params instanceof String) return params.trim()
+}
+
+// 5.使用类型陈述语句收窄
+function test3 (params: Fish | Bird): params is Fish {
+  if ((params as Fish).swim !== undefined) return true
+  else return false
+}
+
+function test4 (params: Fish | Bird) {
+  if (test3(params)) return params.swim
+}
