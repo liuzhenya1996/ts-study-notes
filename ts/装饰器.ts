38bf2eadee11a8a4a3af0465c5af018c312dd8ba
刘振亚 <1965350918@qq.com>
2023-05-06 21:05:18 +0800

    装饰器

---
+++ ts/装饰器.ts
@@ -0,0 +1,11 @@
+// 装饰器这个概念太抽象了，他的目的是给类扩充属性或者改写属性，但是实际上我们扩充类的属性可以用继承的方式更简单易懂的去实现
+// TypeScript 是基于第一版实现了自己的装饰器特性，并标明为实验性质
+// JavaScript 的装饰器还在提案中（提案了好久），还没进入正式标准，掌握半成品实在性价比不高
+// 扔几篇文章在这儿，后面用到的时候再看吧，关于装饰器实在没兴趣做笔记
+/** 
+ * TypeScript 的装饰器有哪些？
+ * https://blog.csdn.net/fe_watermelon/article/details/126918634
+ * 
+ * 一文带你了解TypeScript装饰器
+ * https://www.dandelioncloud.cn/article/details/1609579267291824129
+*/
